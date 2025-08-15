7c5a5af2d372b78839493b66617402320fae43c3
Angel Diaz <angel.diaz@strongmind.com>
2025-08-14 19:20:35 -0700

    (B) main: add perf controller

---
+++ app/controllers/perf_controller.rb
@@ -0,0 +1,27 @@
+class PerfController < ApplicationController
+  include ActionController::Live
+
+  def health
+    render json: { ok: true, ts: Time.now.to_f }
+  end
+
+  # Simulate I/O wait (not CPU). Param s = seconds.
+  def slow
+    delay = params.fetch(:s, "0.2").to_f
+    sleep delay
+    render json: { ok: true, slept: delay }
+  end
+
+  # Chunked stream / simple SSE-ish output to visualize multiplexing
+  def stream
+    response.headers['Content-Type'] = 'text/event-stream'
+    begin
+      10.times do |i|
+        response.stream.write "data: tick #{i} @ #{Time.now.to_f}\n\n"
+        sleep 0.25
+      end
+    ensure
+      response.stream.close
+    end
+  end
+end
